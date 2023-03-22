a3f5e960f629c892b9343d71cb5806b008a3f753
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-22 15:40:55 +0300

    Done

---
+++ Task34/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [100, 1000]
+}
+int CountNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(CountNumbers(array));
---
+++ Task36/Program.cs
@@ -0,0 +1,10 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [100, 1000]
+}
+
+
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
