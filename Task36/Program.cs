fdb1eb0df21271f9f0a7a490622b11efa8b6fe70
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-24 11:30:18 +0300

    Done

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,10 +1,23 @@
 ﻿void InputArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(100, 1000); // [100, 1000]
+        array[i] = new Random().Next(1, 1000);
 }
 
+int SumOdd_i(int[] array)
+{
+    int sum=0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum=sum+array[i];
+    }
+    return sum;
+}
 
-int[] array = new int[10];
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
 InputArray(array);
-Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.Write($"[{string.Join(", ", array)}]");
+Console.WriteLine($" -> {SumOdd_i(array)}");
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * (100 - 10) + 10, 2);
+}
+
+double DiffMaxMin(double[] array)
+{
+    double min=array[0];
+    double max=array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]<min) min=array[i];
+        if (array[i]>max) max=array[i];
+    }
+    return max-min;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+double[] array = new double[n];
+InputArray(array);
+Console.Write($"[{string.Join(", ", array)}]");
+Console.WriteLine($" -> {DiffMaxMin(array)}");
